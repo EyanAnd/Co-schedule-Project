27f32c6f96747e9d8cbcc8645f4b2aeed405a4de
Eyan Anderson <eyananderson@gmail.com>
2023-08-08 10:58:08 -0500

    added reducers and user saga

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,14 @@
 import { combineReducers } from "redux";
+// import other reducers
+import userReducer from "./user.reducer";
+import favsReducer from "./favs.reducer";
+import searchReducer from "./search.reducer";
 
 const rootReducer = combineReducers({
     // reducers here
+    User: userReducer, // holds user data
+    Favorites: favsReducer, // holds user favorites list data
+    Search: searchReducer, // holds search results data
 });
 
 export default rootReducer;
---
+++ src/redux/sagas/user.saga.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { application, json } from "express";
+import { put, takeLatest } from 'redux-saga/effects';
+
+export default function* userSaga() {
+    // put other generator functions in here for the user saga
+    yield takeLatest('FETCH_USER', fetchUser);
+};
+
+function* fetchUser(action) {
+
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true // credentials allow the user to be recognized by the session
+            // so when logged in it will returnt their information
+        }
+        const response = axios.get('/user', config);
+        yield put({ type: 'SET_USER', payload: response.data}); // update the reducer with the logged in user information
+    } catch (err) {
+        console.log('there was an error fetching the user ', err)
+    }
+
+}
